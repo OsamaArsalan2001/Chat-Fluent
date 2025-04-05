cfd73c94cc5edc57cd45803ed35c2bab59670aab
OsamaHelal <ma251306@debi.eui.edu.eg>
2025-04-05 13:28:52 +0200

    don't push that commit

--- app/src/main/java/com/example/chat_fluent/LoginPage.kt
+++ app/src/main/java/com/example/chat_fluent/LoginPage.kt
@@ -1,10 +1,20 @@
 package com.example.chat_fluent
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun LoginScreen(navController: NavController){
+    Column(
+        modifier = Modifier.fillMaxSize() , verticalArrangement = Arrangement.SpaceEvenly , horizontalAlignment = Alignment.CenterHorizontally  ){
 
+
+ }
 }
